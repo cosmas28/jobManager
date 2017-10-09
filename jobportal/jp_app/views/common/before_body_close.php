6c6620358d366aa29c0c5160f3e3915069a4e668
cosmas28 <augubilla100@gmail.com>
2017-10-09 15:09:55 +0300

    add js assets links

--- jobportal/jp_app/views/common/before_body_close.php
+++ jobportal/jp_app/views/common/before_body_close.php
@@ -1,14 +1,13 @@
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
-<script src="<?php echo base_url('public/js/jquery-1.11.0.js');?>"></script> 
-<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="<?php echo base_url('public/js/jquery.js');?>"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="<?php echo base_url('public/js/bootstrap.min.js');?>"></script>
-<script src="<?php echo base_url('public/js/bootbox.min.js');?>"></script>
+<script src="<?php echo base_url('public/js/plugins.js');?>"></script>
+
+<!-- Footer Scripts -->
 <script src="<?php echo base_url('public/js/functions.js');?>" type="text/javascript"></script>
-<script src="<?php echo base_url('public/js/validation.js');?>" type="text/javascript"></script>
-
-
-
-
-
-
-
+<script type="text/javascript">
+  $(function() {
+    $( "#side-navigation" ).tabs({ show: { effect: "fade", duration: 400 } });
+  });
+</script>
