9d2f586e3aba0998af03613949f4beaf94f65f03
cosmas28 <augubilla100@gmail.com>
2017-10-09 15:07:32 +0300

    add assets file

--- jobportal/jp_app/views/common/before_head_close.php
+++ jobportal/jp_app/views/common/before_head_close.php
@@ -1,13 +1,15 @@
-<link href="https://fonts.googleapis.com/css?family=Raleway:400,600,700" rel="stylesheet">
+<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
 
-<link href='<?php echo base_url('public/css/google-fonts.css'); ?>' rel='stylesheet' type='text/css'>
-<link href='<?php echo base_url('public/css/flexslider.css'); ?>' rel='stylesheet' type='text/css'>
+<link href='<?php echo base_url('public/newstatic/css/bootstrap.css'); ?>' rel='stylesheet' type='text/css'>
+<link href='<?php echo base_url('public/newstatic/css/style.css'); ?>' rel='stylesheet' type='text/css'>
+<link href='<?php echo base_url('public/newstatic/css/dark.css'); ?>' rel='stylesheet' type='text/css'>
+<link href='<?php echo base_url('public/newstatic/css/font-icons.css'); ?>' rel='stylesheet' type='text/css'>
+<link href='<?php echo base_url('public/newstatic/css/animate.css'); ?>' rel='stylesheet' type='text/css'>
+<link href='<?php echo base_url('public/newstatic/css/magnific-popup.css'); ?>' rel='stylesheet' type='text/css'>
+<link href='<?php echo base_url('public/newstatic/css/responsive.css'); ?>' rel='stylesheet' type='text/css'>
+<link href="<?php echo base_url('public/newstatic/css/custom.css'); ?>" rel='stylesheet' type='text/css'>
 
-<!-- Bootstrap -->
-<link href="<?php echo base_url('public/css/bootstrap.min.css'); ?>" rel="stylesheet">
-<link href="<?php echo base_url('public/css/font-awesome.css');?>" rel="stylesheet" type="text/css" />
-
-<link href="<?php echo base_url('public/css/style.css'); ?>" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -16,6 +18,3 @@
       <script src="<?php //echo base_url('public/js/respond.min.js');?>"></script>
     <![endif]-->
 <link rel="shortcut icon" href="<?php echo base_url('public/images/favicon.ico'); ?>">
-<script>
-var baseUrl = '<?php echo base_url();?>';
-</script>
