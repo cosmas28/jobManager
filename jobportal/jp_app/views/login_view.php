7cdd55492d25625d7e46e53593ba7bdb50422e18
cosmas28 <augubilla100@gmail.com>
2017-10-10 13:26:21 +0300

    add login view file

--- jobportal/jp_app/views/login_view.php
+++ jobportal/jp_app/views/login_view.php
@@ -1,81 +1,75 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<?php $this->load->view('common/meta_tags'); ?>
-<title><?php echo $title;?></title>
-<?php $this->load->view('common/before_head_close'); ?>
-</head>
-<body>
-<?php $this->load->view('common/after_body_open'); ?>
-<div class="siteWraper">
-<!--Header-->
-<?php $this->load->view('common/header'); ?>
-<!--/Header--> 
-<!--Detail Info-->
-<div class="container innerpages">
- <?php $this->load->view('common/bottom_ads');?>
- 
-  <div class="row"> 
-    
-    <!--Signup-->
-    <div class="col-md-6 col-md-offset-3">
-     <!--Login-->
-    <form name="login_form" id="login_form" action="" method="post">
-      <div class="loginbox">
-        <h3>Existing User</h3>
-        <?php if($msg):?>
-        	<div class="alert alert-danger"><?php echo $msg;?></div>
-        <?php endif;?>
-        <?php echo validation_errors(); ?>
-        <?php echo $this->session->flashdata('success_msg');?>
-        <div class="row">
-          <div class="col-md-3">
-            <label class="input-group-addon">Email <span>*</span></label>
-          </div>
-          <div class="col-md-9">
-            <input type="text" name="email" id="email" class="form-control" value="<?php echo set_value('email'); ?>" placeholder="Email" />
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-3">
-            <label class="input-group-addon">Password <span>*</span></label>
-          </div>
-          <div class="col-md-9">
-            <input type="password" name="pass" id="pass" autocomplete="off" value="<?php echo set_value('pass'); ?>" class="form-control" placeholder="Password" />
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-3"></div>
-          <div class="col-md-9">
-            <input type="checkbox">
-            Remember My Password</div>
-        </div>
-        <div class="row">
-          <div class="col-md-3"></div>
-          <div class="col-md-9">
-            <input type="submit" value="Sign In" class="btn btn-success" />
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-3"></div>
-          <div class="col-md-9">Forgot Your Password? <a href="<?php echo base_url('forgot');?>">Click Here</a></div>
-        </div>
-      </div>
-    </form>
-    
-      <div class="signupbox">
-        <h4>Not a member yet? Click Below to Sign Up</h4>
-        <a href="<?php echo $signup_link;?>" class="btn btn-success">Sign Up Now</a>
-        <div class="clear"></div>
-      </div>
-    </div>
-    <!--/Login--> 
+<html>
+  <head>
+    <?php $this->load->view('base/meta_tags'); ?>
+    <title><?php echo $title;?></title>
+    <?php $this->load->view('base/before_head_close'); ?>
+  </head>
+  <body class="stretched">
+    <?php $this->load->view('base/after_body_open'); ?>
+    <div class="clearfix" id="wrapper">
+      <!--Header-->
+      <?php $this->load->view('base/page-header'); ?>
+      <!--/Header-->
+      <section id="content">
+        <div class="content-wrap">
+          <div class="container clearfix">
+            <?php $this->load->view('base/bottom_ads');?>
+            <div class="divider divider-short divider-center"><i class="icon-crop"></i></div>
+              <div class="tabs divcenter nobottommargin clearfix" id="tab-login-register" style="max-width: 500px;">
+                <div class="panel panel-default nobottommargin">
+                  <div class="panel-body" style="padding: 40px;">
+                    <form class="nobottommargin" action="index.html" id="login-form" method="post">
+                      <h3>Login to your Account</h3>
+                      <?php if($msg): ?>
+                        <div class="alert alert-danger"><?php echo $msg;?></div>
+                      <?php endif; ?>
+                      <?php echo validation_errors(); ?>
+                      <?php echo $this->session->flashdata('success_msg');?>
+                      <div class="col_full">
+                        <label for="login-form-username">Email:</label>
+                        <input placeholder="Email" type="email" id="email" name="email" value="<?php echo set_value('email'); ?>" class="form-control" />
+                      </div>
 
-  </div>
-</div>
-<?php $this->load->view('common/bottom_ads');?>
-<!--Footer-->
-<?php $this->load->view('common/footer'); ?>
-<?php $this->load->view('common/before_body_close'); ?>
-</body>
+                      <div class="col_full">
+                        <label for="login-form-password">Password:</label>
+                        <input type="password" id="pass" name="pass" value="<?php echo set_value('pass'); ?>" placeholder="Password" class="form-control" />
+                      </div>
+                      <div class="col_full">
+    										<input id="checkbox-10" class="checkbox-style" name="checkbox-10" type="checkbox" checked>
+    										<label for="checkbox-10" class="checkbox-style-3-label">Remember My Password</label>
+    									</div>
+
+                      <div class="col_full nobottommargin">
+                        <button type="submit" class="button button-3d button-black nomargin" id="login-form-submit" name="login-form-submit" value="login">Login</button>
+                        <a href="#" class="fright">Forgot Password?</a>
+    									</div>
+                    </form>
+                    <div class="line line-sm"></div>
+                    <div class="center">
+    									<h4 style="margin-bottom: 15px;">Or create a new account:</h4>
+    									<a href="<?php echo $signup_link;?>" class="button button-rounded si-facebook si-colored">Job Seeker</a>
+    									<span class="hidden-xs">or</span>
+    									<a href="<?php echo base_url('employer-signup');?>" class="button button-rounded si-twitter si-colored">Employer</a>
+    								</div>
+                  </div>
+                </div>
+              </div>
+              <div class="divider divider-short divider-center"><i class="icon-crop"></i></div>
+
+              <?php $this->load->view('base/bottom_ads');?>
+          </div>
+        </div>
+      </section>
+      <!--Footer-->
+      <?php $this->load->view('base/footer'); ?>
+      <!--Footer End-->
+    </div>
+    <?php $this->load->view('base/before_body_close'); ?>
+    <script type="text/javascript">
+      $(function() {
+        $( "#side-navigation" ).tabs({ show: { effect: "fade", duration: 400 } });
+      });
+    </script>
+  </body>
 </html>
