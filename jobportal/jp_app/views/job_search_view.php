5ac9ceb447219ed91f45cef5b7ab926ee8eb73ba
cosmas28 <augubilla100@gmail.com>
2017-10-15 15:59:33 +0300

    add view files

--- jobportal/jp_app/views/job_search_view.php
+++ jobportal/jp_app/views/job_search_view.php
@@ -1,131 +1,158 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<?php $this->load->view('common/meta_tags'); ?>
-<meta name="keywords" content="<?php echo $param;?> Jobs" />
-<meta name="description" content="<?php echo $param;?> Jobs ,Find best Jobs. Jobs at <?php echo SITE_NAME;?>." />
-<title><?php echo $title;?></title>
-<?php $this->load->view('common/before_head_close'); ?>
-</head>
-<body>
-<?php $this->load->view('common/after_body_open'); ?>
-<div class="siteWraper">
-<!--Header-->
-<?php $this->load->view('common/header'); ?>
-<!--/Header--> 
-<!--Search Block-->
-<div class="top-colSection">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="candidatesection">          
-          <div class="col-md-9">            
-            <?php echo form_open_multipart('job_search',array('name' => 'jsearch', 'id' => 'jsearch'));?>            
-            <div class="input-group">      
-       		<input type="text" name="job_params" id="job_params" class="form-control" placeholder="Job title or Skill" value="<?php echo $param;?>" />
-              <span class="input-group-btn">
-                 <input type="submit" name="job_submit" class="btn" id="job_submit" value="Find" />
-              </span>
-            </div>            
-            <?php echo form_close();?> </div>
-            <div class="col-md-3">           
-            <input type="submit" value="Upload Resume" title="job search engine" class="postjobbtn" alt="job search engine" onClick="document.location='<?php echo base_url('login');?>'" />
-            <div class="clear"></div>
-          </div>            
-          <div class="clear"></div>
-        </div>
-      </div>
-      <div class="clear"></div>
-    </div>
-  </div>
-</div>
-<!--/Search Block--> 
-<!--Latest Jobs Block-->
-<div class="innerpageWrap">
-<div class="container">
-  <div class="row"> 
-    
-    <!--Left Col-->
-    
-    <?php 
-  $col = '10';
-	if($this->uri->segment(1)!='search'):
-	if($result):
-		$col = '7';
-  		$this->load->view('common/left_job_search');
-	endif;
-	endif;
-	?>
-    
-    <!--Mid Col-->
-    
-    <div class="searchjoblist col-md-<?php echo $col;?>"> 
-      
-      <!--Jobs List-->
-      
-      <div class="searchpage">
-        <div class="toptitlebar">
+<html>
+  <head>
+    <?php $this->load->view('base/meta_tags'); ?>
+    <meta name="keywords" content="<?php echo $param;?> Jobs" />
+    <meta name="description" content="<?php echo $param;?> Jobs ,Find best Jobs. Jobs at <?php echo SITE_NAME;?>." />
+    <title><?php echo $title;?></title>
+    <?php $this->load->view('base/before_head_close'); ?>
+  </head>
+  <body class="stretched">
+    <?php $this->load->view('base/after_body_open'); ?>
+    <div class="clearfix" id="wrapper">
+      <!--Header-->
+      <?php $this->load->view('base/page-header'); ?>
+      <!--/Header-->
+      <!-- job search section -->
+      <div class="section parallax dark notopmargin noborder" style="padding: 80px 0; background-image: url(<?php echo base_url('public/images/webdesign/search-1.png');?>);center center no-repeat; background-size: 100% 400px;background-attachment: fixed;" data-stellar-background-ratio="0.4">
+        <div class="container center clearfix">
           <div class="row">
-            <div class="col-md-6"><b><?php echo $param;?> Jobs</b></div>
-            <div class="col-md-6 text-right"><strong>Jobs <?php echo $from_record.' - '.$page;?> of <?php echo $total_rows;?></strong> </div>
+            <?php echo form_open_multipart('job_search',array('name' => 'jsearch', 'id' => 'jsearch'));?>
+            <div class="col-md-10 col-xs-8">
+              <div class="input-fld">
+    						<input type="text" placeholder="Job title or Skill" name="job_params" id="job_params" value="<?php echo $param;?>" />
+              </div>
+            </div>
+            <div class="col-md-2 col-xs-4">
+              <div class="input-fld">
+    						<button type="submit" name="job_submit" id="job_submit" value="Find" />Search</button>
+    					</div>
+            </div>
+            <?php echo form_close();?> </div>
           </div>
         </div>
-        
-        
-        <ul class="searchlist">
-        <!--Job Row-->
-          
-          <?php if($result):
-		$CI =& get_instance();
-				  			foreach($result as $row):
-								$company_logo = ($row->company_logo)?$row->company_logo:'no_logo.jpg';
-								$is_already_applied = $CI->is_already_applied_for_job($this->session->userdata('user_id'), $row->ID);		
-				  ?>
-          <li>
-            <div class="row">
-              <div class="col-md-2"><a href="<?php echo base_url('jobs/'.$row->job_slug);?>" class="thumbnail" title="<?php echo $row->job_title;?>"><img src="<?php echo base_url('public/uploads/employer/thumb/'.$company_logo);?>" alt="<?php echo base_url('company/'.$row->company_slug);?>" /></a></div>
-              <div class="col-md-10">
-                <div class="col-md-7"> <a href="<?php echo base_url('jobs/'.$row->job_slug);?>" class="jobtitle" title="<?php echo $row->job_title;?>"><?php echo word_limiter(strip_tags(str_replace('-',' ',$row->job_title)),7);?></a>
-                  <div class="location"><a href="<?php echo base_url('company/'.$row->company_slug);?>" title="Jobs in <?php echo $row->company_name;?>"><?php echo $row->company_name;?></a> &nbsp;-&nbsp; <?php echo $row->city;?></div>
-                  <div class="date"><?php echo date_formats($row->dated, 'M d, Y');?></div>
-                </div>
-                <div class="col-md-5">
+      <!-- ./job search section -->
+
+      <!-- Content begins -->
+      <div id="content">
+        <div class="content-wrap">
+          <div class="container clearfix">
+            <!-- left sidebar -->
+            <div class="sidebar nobottommargin clearfix">
+              <div class="sidebar-widgets-wrap">
+                <div class="widget clearfix">
                   <?php
-			  	if($is_already_applied=='yes'):
-			  ?>
-                  <a href="javascript:;" class="applybtngray">Already Applied</a>
-                  <?php else:?>
-                  <a href="<?php echo base_url('jobs/'.$row->job_slug.'?apply=yes');?>" class="applybtn">Apply Now</a>
-                  <?php endif;?>
-                  
+                  $col = '';
+                  if($this->uri->segment(1)!='search'):
+                    if($result):
+                      $col = ' bothsidebar';
+                    	$this->load->view('base/left_job_search');
+                    endif;
+                  endif;
+                   ?>
                 </div>
-                <div class="clearfix"> </div>               
-              </div>              
+              </div>
             </div>
-             <p><?php echo word_limiter(strip_tags(str_replace('-',' ',$row->job_description)),22);?></p>
-          </li>
-          <?php 
-				  			endforeach;
-							else: ?>
-          <div class="err" align="center">
-            <p><strong> <?php echo ($param=='')?'Please enter keywords above to display the relevant opened jobs.':'Sorry, no record found';?> </strong></p>
+            <!-- ./left sidebar -->
+
+            <!-- Post Content -->
+            <div class="postcontent <?php echo $col;?> nobottommargin clearfix">
+              <div class="row">
+  							<div class="fancy-title title-bottom-border">
+  								<h3><span><?php echo $param;?> Jobs</span><p class="fright">Jobs <?php echo $from_record.' - '.$page;?> of <?php echo $total_rows;?></p></h3>
+  							</div>
+  						</div>
+
+              <!--Job Row-->
+              <?php
+                if($result):
+                  $CI =& get_instance();
+                  foreach($result as $row):
+                    $company_logo = ($row->company_logo)?$row->company_logo:'no_logo.jpg';
+                    $is_already_applied = $CI->is_already_applied_for_job($this->session->userdata('user_id'), $row->ID);
+               ?>
+                    <div class="well">
+                      <div class="row">
+                        <div class="col-md-2 col-xs-2">
+                          <div class="job-box">
+                            <a href="<?php echo base_url('jobs/'.$row->job_slug);?>" class="thumbnail" title="<?php echo $row->job_title;?>"><img src="<?php echo base_url('public/uploads/employer/thumb/'.$company_logo);?>" alt="<?php echo base_url('company/'.$row->company_slug);?>" style="height: 80px; width: 100%; display: block;" /></a>
+                          </div>
+                        </div>
+                        <div class="col-md-6 col-xs-6">
+                          <div class="job-box">
+                            <h4><a href="<?php echo base_url('jobs/'.$row->job_slug);?>" title="<?php echo $row->job_title;?>"><?php echo word_limiter(strip_tags(str_replace('-',' ',$row->job_title)),7);?></a>&nbsp;-&nbsp; Ksh 0.00</h4>
+        										<a href="<?php echo base_url('company/'.$row->company_slug);?>" title="Jobs in <?php echo $row->company_name;?>"><?php echo $row->company_name;?></a>&nbsp;-&nbsp; <?php echo $row->city;?>
+        										<h4><?php echo date_formats($row->dated, 'M d, Y');?></h4>
+                          </div>
+                        </div>
+                        <div class="col-md-4 col-xs-4">
+                          <div class="job-box">
+                            <?php
+                            if($is_already_applied=='yes'):
+                             ?>
+                              <a href="javascript:;" class="job-box-btn"><i class="icon-ok"></i>Already Applied</a>
+                             <?php else:?>
+                               <a href="<?php echo base_url('jobs/'.$row->job_slug.'?apply=yes');?>" class="job-box-btn">Apply Now</a>
+                              <?php endif;?>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-12">
+                          <p><?php echo word_limiter(strip_tags(str_replace('-',' ',$row->job_description)),22);?></p>
+                        </div>
+                      </div>
+                    </div>
+               <?php
+               endforeach;
+             else:
+                ?>
+                <div class="divcenter nobottommargin clearfix">
+                  <p><strong> <?php echo ($param=='')?'Please enter keywords above to display the relevant opened jobs.':'Sorry, no record found';?> </strong></p>
+                </div>
+                <?php endif;?>
+              <!--./Job Row-->
+              <!--Pagination-->
+              <div class="nobottommargin clearfix">
+                <ul class="pagination">
+                  <li><?php echo ($result)?$links:'';?></li>
+                </ul>
+              </div>
+              <!--./Pagination-->
+            </div>
+            <!-- ./Post Content -->
+
+            <!-- Sidebar -->
+            <div class="sidebar nobottommargin col_last clearfix">
+              <div class="sidebar-widgets-wrap">
+                <!--<div class="row center">
+  								<a href="#" class="thumbnail">
+  								<img alt="100%x180" src="images/extras/200x200.gif" style="height: 700px; width: 100%; display: block;"></a>
+  							</div>
+
+  							<div class="row center">
+  								<a href="#" class="thumbnail">
+  								<img alt="100%x180" src="images/extras/200x200.gif" style="height: 700px; width: 100%; display: block;"></a>
+  							</div>-->
+                <?php $this->load->view('base/right_ads');?>
+              </div>
+            </div>
           </div>
-          <?php endif;?>
-        </ul>
+        </div>
       </div>
-      
-      <!--Pagination-->
-      
-      <div class="paginationWrap"> <?php echo ($result)?$links:'';?> </div>
+      <!-- ./Content begins -->
+      <!--Footer-->
+      <?php $this->load->view('base/footer'); ?>
+      <!--Footer End-->
     </div>
-    <?php $this->load->view('common/right_ads');?>
-  </div>
-</div>
-</div>
-<!--/Latest Jobs Block-->
-<?php $this->load->view('common/bottom_ads');?>
-<!--Footer-->
-<?php $this->load->view('common/footer'); ?>
-<?php $this->load->view('common/before_body_close'); ?>
-</body>
+    <!-- Go To Top
+  	============================================= -->
+  	<div id="gotoTop" class="icon-angle-up"></div>
+    <?php $this->load->view('base/before_body_close'); ?>
+    <script type="text/javascript">
+      $(function() {
+        $( "#side-navigation" ).tabs({ show: { effect: "fade", duration: 400 } });
+      });
+    </script>
+  </body>
 </html>
