61d1734337ec1a4666ca55368b7363364c14617d
cosmas28 <augubilla100@gmail.com>
2017-11-12 14:37:07 -0800

    change static file directory

--- jobportal/jp_app/views/common/before_body_close.php
+++ jobportal/jp_app/views/common/before_body_close.php
@@ -1,10 +1,10 @@
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
-<script src="<?php echo base_url('public/js/jquery-1.11.0.js');?>"></script> 
+<script src="<?php echo base_url('public/oldstatic/js/jquery-1.11.0.js');?>"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed --> 
-<script src="<?php echo base_url('public/js/bootstrap.min.js');?>"></script>
-<script src="<?php echo base_url('public/js/bootbox.min.js');?>"></script>
-<script src="<?php echo base_url('public/js/functions.js');?>" type="text/javascript"></script>
-<script src="<?php echo base_url('public/js/validation.js');?>" type="text/javascript"></script>
+<script src="<?php echo base_url('public/oldstatic/js/bootstrap.min.js');?>"></script>
+<script src="<?php echo base_url('public/oldstatic/js/bootbox.min.js');?>"></script>
+<script src="<?php echo base_url('public/oldstatic/js/functions.js');?>" type="text/javascript"></script>
+<script src="<?php echo base_url('public/oldstatic/js/validation.js');?>" type="text/javascript"></script>
 
 
 
