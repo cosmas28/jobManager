7427f2448f5bafd425e8ba954ed383cf7c3e5985
cosmas28 <augubilla100@gmail.com>
2017-11-21 16:39:05 +0300

    updated 404 page

--- jobportal/jp_app/views/404_view.php
+++ jobportal/jp_app/views/404_view.php
@@ -1,33 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php $this->load->view('common/meta_tags'); ?>
-<title>Page not found</title>
-<?php $this->load->view('common/before_head_close'); ?>
+    <?php $this->load->view('base/meta_tags'); ?>
+    <title>Page not found</title>
+    <?php $this->load->view('base/before_head_close'); ?>
+    <?php $this->load->view('common/before_head_close'); ?>
 </head>
-<body>
-<?php $this->load->view('common/after_body_open'); ?>
-<div class="siteWraper">
-<!--Header-->
-<?php $this->load->view('common/header'); ?>
-<!--/Header--> 
-<!--Detail Info-->
-<div class="container detailinfo">
-  <div class="row">
-    <div class="col-md-12"><!--Job Detail-->
-      
-      <div class="row">
-        <div class="col-md-12 text-center"><a href="<?php echo base_url();?>"><img src="<?php echo base_url() ;?>/public/images/not-found.jpg" /></a></div>
-      </div>
+<body class="stretched">
+<?php $this->load->view('base/after_body_open'); ?>
+<!-- Document Wrapper ============================================= -->
+<div id="wrapper" class="clearfix">
+    <!--Header-->
+    <?php $this->load->view('base/page-header'); ?>
+    <!--/Header-->
+    <!--Detail Info-->
+    <div class="container detailinfo">
+        <div class="row">
+            <div class="col-md-12"><!--Job Detail-->
+
+                <div class="row">
+                    <div class="col-md-12 text-center"><a href="<?php echo base_url();?>"><img src="<?php echo base_url() ;?>/public/images/not-found.jpg" /></a></div>
+                </div>
+            </div>
+            <!--/Job Detail-->
+
+        </div>
     </div>
-    <!--/Job Detail--> 
-    
-  </div>
+    <?php $this->load->view('common/bottom_ads');?>
+    <!--Footer-->
+    <?php $this->load->view('base/footer'); ?>
 </div>
-</div>
-<?php $this->load->view('common/bottom_ads');?>
-<!--Footer-->
-<?php $this->load->view('common/footer'); ?>
+<!-- Go To Top
+	============================================= -->
+<div id="gotoTop" class="icon-angle-up"></div>
+<?php $this->load->view('base/before_body_close'); ?>
 <?php $this->load->view('common/before_body_close'); ?>
 </body>
 </html>
