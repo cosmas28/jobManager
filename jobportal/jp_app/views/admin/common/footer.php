d722e6d714e147e405b5fd17c880ed5a81b5a278
cosmas28 <augubilla100@gmail.com>
2017-11-22 18:10:31 +0300

    change js and css path

--- jobportal/jp_app/views/admin/common/footer.php
+++ jobportal/jp_app/views/admin/common/footer.php
@@ -2,11 +2,11 @@
 </div>
 <!-- ./wrapper -->
 <?php if($this->uri->segment(2)!='skills'):?>
-<script src="<?php echo base_url('public/js/jquery-1.9.1.js'); ?>"></script>
-<script src="<?php echo base_url('public/js/jquery-ui.js'); ?>"></script>
-<script src="<?php echo base_url('public/js/admin/bootstrap.min.js');?>" type="text/javascript"></script>
-<script src="<?php echo base_url('public/js/admin/admin_functions.js');?>" type="text/javascript"></script>
-<script src="<?php echo base_url('public/js/validation.js');?>" type="text/javascript"></script>
+<script src="<?php echo base_url('public/oldstatic/js/jquery-1.9.1.js'); ?>"></script>
+<script src="<?php echo base_url('public/oldstatic/js/jquery-ui.js'); ?>"></script>
+<script src="<?php echo base_url('public/oldstatic/js/admin/bootstrap.min.js');?>" type="text/javascript"></script>
+<script src="<?php echo base_url('public/oldstatic/js/admin/admin_functions.js');?>" type="text/javascript"></script>
+<script src="<?php echo base_url('public/oldstatic/js/validation.js');?>" type="text/javascript"></script>
 <!--<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>-->
 <?php endif;?>
 </body>
