d1d2944d1f1ba0af2b5accda2ef50e33b26bf9df
cosmas28 <augubilla100@gmail.com>
2017-11-22 18:27:45 +0300

    developed admin header

--- jobportal/jp_app/views/admin/common/header.php
+++ jobportal/jp_app/views/admin/common/header.php
@@ -1,4 +1,4 @@
-<header class="header"> <a href="<?php echo base_url();?>admin/dashboard" class="logo"> Job Portal </a>
+<header class="header"> <a href="<?php echo base_url();?>admin/dashboard" class="logo"> TASKjet </a>
   <nav class="navbar navbar-static-top" role="navigation"> <a href="#" class="navbar-btn sidebar-toggle" data-toggle="offcanvas" role="button"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a>
     <div class="navbar-right">
       <ul class="nav navbar-nav">
