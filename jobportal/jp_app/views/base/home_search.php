3d4854aec4c5c8e80d7ad1995f4032e4e669baf6
cosmas28 <augubilla100@gmail.com>
2017-11-23 19:34:27 +0300

    updated home_view

--- jobportal/jp_app/views/base/home_search.php
+++ jobportal/jp_app/views/base/home_search.php
@@ -1,8 +1,6 @@
 <?php if($this->session->userdata('is_user_login')!=TRUE): ?>
   <div class="heading-block title-center nobottomborder">
     <h1>Hire &amp; Get Hiring-Holder</h1>
-    <span>Each month, more than 7 million jobseekers turn to
-    website in their search for work, making over 160,000 applications every day.</span>
   </div>
   <div class="center bottommargin">
     <?php echo form_open_multipart('job_search/search',array('name' => 'jsearch', 'id' => 'jsearch'));?>
